c652dca1427511f1b95a887e1bc151eb6838323d
Temuri Takalandze <takalandzet@gmail.com>
2021-01-04 00:37:25 +0400

    :sparkles: Implement feature to change Task status from details fragment.

--- app/src/main/java/dev/abgeo/todo/TaskDetailsFragment.kt
+++ app/src/main/java/dev/abgeo/todo/TaskDetailsFragment.kt
@@ -5,14 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
 import androidx.navigation.findNavController
 import androidx.navigation.navGraphViewModels
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class TaskDetailsFragment : Fragment() {
 
@@ -38,6 +35,21 @@ class TaskDetailsFragment : Fragment() {
                 cbTaskTitle.text = title
                 cbTaskTitle.isChecked = isCompleted
                 tvTaskBody.text = body
+
+                cbTaskTitle.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
+                    task?.let { t ->
+                        run {
+                            t.isCompleted = isChecked
+                            context?.let { c -> TaskRepository.updateTask(c, t) }
+                            Snackbar.make(
+                                view,
+                                getText(if (isChecked) R.string.mark_as_completed else R.string.mark_as_active),
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+                }
             }
         })
 
